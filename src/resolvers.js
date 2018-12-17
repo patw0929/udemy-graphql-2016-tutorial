de54b6167420938356fabdd4188e9f90e2a60410
patw <patw.hi@gmail.com>
2018-12-18 00:56:15 +0800

    Read author data from db

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,40 +1,13 @@
 import mongoose from 'mongoose';
 import authorModel from '../models/author';
 
-const authors = [
-  {
-    id: 21,
-    name: 'JK Rowling',
-    age: 50,
-    books: [
-      'Harry Potter and the Goblet of Fire',
-      'Harry Potter and the Prisoner of Azkaban',
-    ],
-  },
-  {
-    id: 22,
-    name: 'Georage RR Martin',
-    age: 70,
-    books: [
-      'GOT - Song of Ice and Fire',
-      'GOT - A Dance with Dragons',
-    ],
-  },
-  {
-    id: 23,
-    name: 'Stephen King',
-    age: 60,
-    books: ['The Green Mile', 'Carrie']
-  },
-];
-
 const resolvers =  {
   Query: {
     authors: () => {
-      return authors;
+      return authorModel.find({});
     },
     author: (root, { id }) => {
-      return authors.find(author => author.id === id);
+      return authorModel.findOne({ id });
     },
   },
   Mutation: {
